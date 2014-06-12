188f07ba79c929a3f140890a2edcd2b0e4639b5e
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-12 12:07:46 +0100

    release version 0.2.0

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -7,7 +7,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   val nameApp = "reactivemongo-json"
-  val versionApp = "0.2.0-SNAPSHOT"
+  val versionApp = "0.2.0"
 
   val appDependencies = {
     import Dependencies._
