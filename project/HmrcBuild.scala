95aabd3b62c53794f2b6f0b6d5d7566257db629a
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-12 12:14:43 +0100

    increment version 0.3.0-SNAPSHOT

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -7,7 +7,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.DefaultBuildSettings
 
   val nameApp = "reactivemongo-json"
-  val versionApp = "0.2.0"
+  val versionApp = "0.3.0-SNAPSHOT"
 
   val appDependencies = {
     import Dependencies._
